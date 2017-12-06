9fca5284f3665a40de75453b6b9f7b275309df39
Jeremy <hurleyjer@yahoo.com>
2017-12-06 15:25:28 -0500

    working 3A

---
+++ 3/3.js
@@ -0,0 +1,16 @@
+let input = 361527;
+let square = 0;
+
+while (square * square < input) {
+    square += 1;
+}
+
+let distance = Math.floor(square / 2);
+let position = input - (square - 1) * (square - 1);
+
+if (position > square) {
+    position -= (square - (square % 2));
+}
+
+//console.log(square, distance, position);
+console.log(distance + Math.abs(Math.ceil((square) / 2) - position))
