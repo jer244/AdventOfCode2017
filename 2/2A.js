14496440eb901f8dd017f764eb4b43bb126970b3
Jeremy <hurleyjer@yahoo.com>
2017-12-06 13:42:04 -0500

    working 2B

--- 2/2A.js
+++ 2/2A.js
@@ -7,11 +7,33 @@ input = fs.readFileSync('./input.txt')
     .toString()
     .split('\r\n');
 
-input.forEach((e)=>{
+input.forEach((e) => {
     let row = e.split('\t');
-    let min = row.reduce((a,b)=> {return Math.min(a, b)});
-    let max = row.reduce((a,b)=> {return Math.max(a, b)});
-    sum += max - min;    
+    let min = row.reduce((a, b) => { return Math.min(a, b) });
+    let max = row.reduce((a, b) => { return Math.max(a, b) });
+    sum += max - min;
 })
 
 console.log('2A = ' + sum);
+
+let sumB = 0;
+input.forEach((e) => {
+    let row = e.split('\t');
+    let value = 0;
+    for (let i = 0; i < row.length; i++) {
+        for (let j = 0; j < row.length; j++) {
+            if (i != j) {
+                if (row[i] % row[j] == 0) {
+                    value = row[i] / row[j];
+                    break;
+                }
+            }
+        }
+        if (value) {
+            sumB += value;
+            break;
+        }
+    }
+})
+
+console.log('2B = ', sumB);
