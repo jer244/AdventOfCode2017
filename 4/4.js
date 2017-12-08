8d73f02ff10791e7ffbf09c1de7a8f5d650c6c0d
Jeremy <hurleyjer@yahoo.com>
2017-12-07 21:15:18 -0500

    working 4B

--- 4/4.js
+++ 4/4.js
@@ -1,18 +1,27 @@
 const fs = require('fs');
 
 let input;
-let count = 0;
+let countA = 0;
+let countB = 0;
 
 input = fs.readFileSync('./input4.txt')
     .toString()
     .split('\r\n');
 
-input.forEach((e) => {
-    let row = e.split(' ');
-    row.sort();
-    if (row.find((e, i) => { return e == row[i + 1]; }) == undefined) {
-        count++;
-    }
+input.forEach((e, i) => {
+    input[i] = e.split(' ')
 })
 
-console.log(count);
+input.forEach((row) => {
+    row.sort();
+    if (row.find((e, i) => { return e == row[i + 1]; }) == undefined) {
+        countA++;
+    };
+    let sorted = row.map(e => e.split('').sort().join(''));
+    sorted.sort();
+    if (sorted.find((e, i) => { return e == sorted[i + 1]; }) == undefined) {
+        countB++;
+    };
+})
+
+console.log('4A = ' + countA + ' 4B = ' + countB);
