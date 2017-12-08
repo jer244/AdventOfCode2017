8be2c4ebb7996f61b7eedb90aa7023b024633b58
Jeremy <hurleyjer@yahoo.com>
2017-12-07 23:40:23 -0500

    working 6A

---
+++ 6/6.js
@@ -0,0 +1,27 @@
+let bank = [5, 1, 10, 0, 1, 7, 13, 14, 3, 12, 8, 10, 7, 12, 0, 6];
+let perms = [];
+
+while (perms.indexOf(bank.join()) == -1) {
+    perms.push(bank.join());
+    move(bank);
+}
+
+console.log(perms.length);
+
+
+
+function move(arr) {
+    let max = arr.reduce((a, b) => Math.max(a, b))
+    let i = arr.findIndex( e => e == max);
+
+    arr[i] = 0;
+
+    while (max > 0) {
+        i++;
+        if (i > arr.length - 1) {
+            i = 0;
+        }
+        arr[i]++;
+        max--;
+    }
+}
